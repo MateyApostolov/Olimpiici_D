4c154128ebcaa397d9e2adc947aed1175ab9996a
MateyApostolov <mateyapostolov@gmail.com>
2024-10-30 18:46:34 +0200

    asd

--- prime.cpp
+++ prime.cpp
@@ -1,63 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = 1000000000;
-bool er[maxn];
-vector <int> prd;
-vector <int> del;
+vector <pair<int, int>> del;
 
-void eratosten () {
-    for(int d = 2; d < maxn; d++) {
-        if(!er[d]) {
-            for(int h = d * d; h < maxn; h++) {
-                er[h] = true;
-            }
-        }
+bool is_prime (int num) {
+    if(num < 2) return false;
+    double koren = sqrt(num);
+    for(int i = 2; i <= koren; i++) {
+        if(num % i == 0) return false;
     }
-    return;
+    return true;
 }
 
-void find_del (int num) {
-    int i;
-    for(i = 0; i * i < num; i++) {
-        if(num % i == 0) {
-            del.push_back(i);
-            del.push_back(num / i);
-        }
-    }
-    if(i * i == num) {
-        del.push_back(i);
-    }
-    return;
-}
-
-void set_pdel (int num) {
-    for(int i = 2; i < maxn; i++) {
-        if(!er[i]) {
-            prd.push_back(i);
-        }
-    }
-
-}
-
-vector <int> kan_razlagane (int num) {
-    int h = 0;
-    vector <int> kand;
+void kan_razlagane (int num) {
+    int h = 2;
+    map <int, int> mp;
     while(num > 1) {
-        if(num % prd[h] == 0) {
-            kand.push_back(prd[h]);
-            num /= prd[h];
+        if(is_prime(num)) {
+            mp[num]++;
+            break;
+        }
+        if(is_prime(h)) {
+            while(num % h == 0) {
+                num /= h;
+                mp[h]++;
+            }
         }
         h++;
     }
-    return kand;
+    for(auto x : mp) {
+        del.push_back({x.first, x.second});
+    }
+    return;
 }
 
 int main () {
-    int n;
+    int n, j;
     cin >> n;
-    find_del(n);
-    eratosten ();
+    kan_razlagane(n);
+    int k = del.size();
+    int r[k] = {};
+    bool p[k] = {};
+    while(true) {
+        int d = 1;
+        for(int i = 0; i < k; i++) {
+            d *= pow(del[i].first, r[i]);
+        }
+        cout << d << ' ';
+        for(j = 0; j < k; j++) {
+            if(j != k && p[j]) {
+                r[j]--;
+                if(r[j] >= 0) break;
+                else {
+                    r[j] = 0;
+                    p[j] = false;
+                }
+            } else {
+                r[j]++;
+                if(r[j] <= del[j].second) break;
+                else {
+                    r[j]--;
+                    p[j] = true;
+                }
+            }
+        }
+        if(j >= k) return 0;
+    }
+
 
 
     return 0;
