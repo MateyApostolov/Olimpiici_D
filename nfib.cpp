34ecc19bdfb5fc89deb6c8d9e6565fe3b274fd86
MateyApostolov <mateyapostolov@gmail.com>
2025-02-27 20:49:16 +0200

    asd

--- nfib.cpp
+++ nfib.cpp
@@ -1,1 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main () {
+    int n, k, m, nc;
+    cin >> n >> k >> m;
+    vector <int> fib;
+    for(int i = 0; i < n; i++) {
+        cin >> nc;
+        fib.push_back(nc % m);
+    }
+    for(int i = 0; i < n; i++) {
+        cin >> nc;
+        fib[i] = (fib[i] * nc) % m;
+    }
+    if(k <= n) {
+        cout << fib[k - 1] % m;
+        return 0;
+    }
+    for(int i = n; i < k; i++) {
+        fib.push_back(0);
+        bool t = true;
+        for(int x = 1; x <= n; x++) {
+            fib[fib.size() - 1] = (fib[fib.size() - 1] + fib[fib.size() - x - 1]) % m;
+        }
+        for(int x = 1; x <= n; x++) {
+            if(fib[x] != fib[fib.size() - x - 1]) t = false;
+        }
+        if(t) {
+            for(int i = 0; i < n; i++) fib.pop_back();
+            if(k % fib.size() == 0) {
+                cout << fib[fib.size() - 1];
+            } else {
+                cout << fib[(k % fib.size()) - 1];
+            }
+            return 0;
+
+        }
+    }
+    cout << fib[fib.size() - 1];
+    return 0;
+}
--- shots.cpp
+++ shots.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main () {
-    int n, m;
-    cin >> n >> m;
+const int maxn = 5001;
+bool sh[maxn][maxn];
 
+int main () {
+    int n, m, ans = 1;
+    cin >> n >> m;
+    for(int i = 1; i < n; i++) {
+        for(int x = 0; x < m; x++) {
+            if(sh[i][x]) continue;
+            ans++;
+            int hi = i, hx = x;
+            while(hi < n && hx < m) {
+                sh[hi][hx] = true;
+                hi += i;
+                hx += x;
+            }
+        }
+    }
+    cout << ans;
     return 0;
 }
