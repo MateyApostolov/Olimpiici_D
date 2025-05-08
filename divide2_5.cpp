4c03d9250edff9bd76a6744d8aacf90203c95a03
MateyApostolov <mateyapostolov@gmail.com>
2025-05-08 20:40:54 +0300

    asd

--- divide2_5.cpp
+++ divide2_5.cpp
@@ -3,47 +3,49 @@ using namespace std;
 
 const int maxn = 2 * pow(10, 6) + 1;
 bool zdr[maxn];
-int brd[maxn];
-
-int brr (int num, int del) {
-    int br = 0;
-    while(num % del == 0) {
-        num /= del;
-        br++;
-    }
-    return br;
-}
+int ngd[maxn];
+map <int, int> del, brd;
 
 void eratosten () {
-    for(int d = 2; d <= maxn; d++) {
+    for(long long d = 2; d <= maxn; d++) {
         if(zdr[d]) continue;
-        for(int h = d * d; h <= maxn; h += d) {
+        ngd[d] = d;
+        for(long long h = d * d; h <= maxn; h += d) {
             zdr[h] = true;
+            ngd[h] = d;
         }
+    }
+    return;
+}
+
+void can (int num) {
+    while(num > 1) {
+        del[ngd[num]]++;
+        num /= ngd[num];
     }
     return;
 }
 
 int main () {
     eratosten();
-    int n, r = -1, op = 0;
+    int n, op = 0;
     cin >> n;
     int nc[n];
-    vector <int> prd;
     for(int i = 0; i < n; i++) cin >> nc[i];
-    for(int i = 2; i <= maxn; i++) {
-        if(!zdr[i]) prd.push_back(i);
+    can(nc[0]);
+    for(auto x : del) {
+        op += x.second;
     }
-    for(int i = 0; i < prd.size(); i++) {
-        for(int l = 0; l < n;) {
-            int maxbr = 0;
-            while(r + 1 < n && nc[r] % prd[i] == 0) {
-                r++;
-                maxbr = max(maxbr, brr(nc[r], prd[i]));
+    brd = del;
+    for(int i = 1; i < n; i++) {
+        del.clear();
+        can(nc[i]);
+        for(auto x : del) {
+            if(brd[x.first] < x.second) {
+                op += x.second - brd[x.first];
             }
-            op += maxbr;
-            l++;
         }
+        brd = del;
     }
     cout << op;
     return 0;
