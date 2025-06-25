eabdbda1d937d32cff84aa4503b6e2ad7eee7e96
MateyApostolov <mateyapostolov@gmail.com>
2025-06-25 17:51:33 +0300

    asd

--- town.cpp
+++ town.cpp
@@ -3,26 +3,36 @@ using namespace std;
 
 const int maxn = 2 * pow(10, 5) + 1;
 vector <pair<int, int>> graph[maxn];
-bool vis[maxn];
 
 int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n, x, y, a, b;
     cin >> n >> x >> y;
+    int ra[n + 1];
+    for(int i = 0; i <= n; i++) ra[i] = INT_MAX;
     while(cin >> a >> b) {
         graph[a].push_back({b, 0});
         graph[b].push_back({a, 1});
     }
     priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> qu;
-    qu.push({x, 0});
+    qu.push({0, x});
+    ra[x] = 0;
     while(!qu.empty()) {
-        pair<int, int> a = qu.top();
+        int d = qu.top().first, idx = qu.top().second;
         qu.pop();
-        for(auto b : graph[a.first]) {
-            if(vis[b.first]) continue;
-            qu.push({b.first, b.second + a.second});
-            vis[b.first] = true;
+        if(d > ra[idx]) continue;
+        for(auto b : graph[idx]) {
+            int nidx = b.first, nd = b.second + d;
+            if(nd < ra[nidx]) {
+                qu.push({nd, nidx});
+                ra[nidx] = nd;
+            }
         }
+
     }
-    if(!vis[y]) cout << 'X';
+    if(ra[y] == INT_MAX) cout << 'X';
+    else cout << ra[y];
     return 0;
 }
