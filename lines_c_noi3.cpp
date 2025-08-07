ab398b180edbc1f7d117349bd228aa08410a675f
MateyApostolov <mateyapostolov@gmail.com>
2025-08-07 19:09:28 +0300

    asd

--- lines_c_noi3.cpp
+++ lines_c_noi3.cpp
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = 1001;
-int mm[maxn][maxn];
-int n, i, j;
+const int maxn = 1001, d7 = pow(10, 9) + 7;
+int mm[maxn][maxn], n, i, j;
 
-int dp (int p, int d) {
-    if(p == d) return 1;
-    ///if(p > d) swap(p, d);
-    if(mm[p][d] != 0) return mm[p][d];
-    int v = 0;
-    for(int x = p + 1; x <= d; x++) {
-        v += dp(p, x);
-        v += dp(x, d);
-    }
-    for(int x = p + 1; x <= n; x++) {
-        v += dp(p, x);
-        v += dp(x, d);
-    }
-    return mm[p][d] = v;
+int dp (int s, int p) {
+    if(s == 2 && p == 2) return 1;
+    if(mm[s][p] != 0) return mm[s][p];
+    long long r = 0;
+    for(int x = 2; x < p; x++) r += dp(s - x + 1, p - x + 1);
+    for(int x = p + 1; x <= s; x++) r += dp(x - 1, x - p + 1);
+    return mm[s][p] = r % d7;
 }
 
 
 int main () {
     cin >> n >> i >> j;
-    cout << dp(i, j);
+    cout << dp(n, j - i + 1);
 
     return 0;
 }
