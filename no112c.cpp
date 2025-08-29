7d77d267d385a534f1dd2ba5abfb7a4407e21f6c
MateyApostolov <mateyapostolov@gmail.com>
2025-08-29 17:58:42 +0300

    asd

--- no112c.cpp
+++ no112c.cpp
@@ -20,20 +20,15 @@ int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n, m, q, u, v;
+    int n, m, t, q, u, v;
     cin >> n >> m;
     for(int i = 1; i <= n; i++) par[i] = i;
     for(int i = 0; i < m; i++) {
-        cin >> u >> v;
-        union_(u, v);
-        find_p(v);
-    }
-    cin >> q;
-    for(int i = 0; i < q; i++) {
-        cin >> u >> v;
-        if(find_p(u) == find_p(v)) cout << 1;
-        else cout << 0;
-    }
+        cin >> t >> u >> v;
+        if(t == 1) union_(u, v);
+        else if(find_p(v) == find_p(u)) cout << "yes\n";
+        else cout << "no\n";
+    }a
 
     return 0;
 }
